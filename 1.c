7bfcd454c39d3a9d5cd0d9bf82e4db3de1a4f319
Figueiredodev777 <rafaelmagalhaesfigue@gmail.com>
2025-04-12 17:01:03 -0400

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+    int a, b, c;
+
+int main(){
+	
+	int a, b, c;
+	
+	printf("Digite o valor de um lado do triangulo:\n");
+	scanf("%d", &a);
+	
+	printf("Digite o valor de um lado do triangulo:\n");
+	scanf("%d", &b);
+	
+	printf("Digite o valor de um lado do triangulo:\n");
+	
+	scanf("%d", &c);
+	
+	if(a == b == c){
+		printf("Triangulo Equilatero!\n");
+}
+	else(a != b != c);{
+		printf("Triangulo escaleno!\n");
+
+}
+    if(a == b != c);{
+        printf("triangulo isoceles!\n");
+
+}
+
+}
