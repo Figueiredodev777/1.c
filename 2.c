4aa9b0f5e0efdb1296bacaf89e7437b692ba2efc
Figueiredodev777 <rafaelmagalhaesfigue@gmail.com>
2025-04-12 17:01:53 -0400

    Add files via upload

---
+++ 2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+float altura;
+int main(){
+	
+	printf("Digite sua altura em metros:\n");
+	scanf("%2.f", &altura);
+	
+		if(altura > 1.75){
+		printf("Altura menor do que a media mundial!\n");
+}
+	else(altura < 1.75);{
+		printf("Altura maior do que a media mundial!\n");
+
+}
+
+} 
